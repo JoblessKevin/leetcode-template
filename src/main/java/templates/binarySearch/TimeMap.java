f27112c94a404e3c3eff5802abc3919ec506dab6
KevinChiu <kevinhyd108@gmail.com>
2025-10-21 13:05:10 +0800

    Add TreeMap solution

--- src/main/java/templates/binarySearch/TimeMap.java
+++ src/main/java/templates/binarySearch/TimeMap.java
@@ -59,3 +59,31 @@ class TimeMap {
  * obj.set(key,value,timestamp);
  * String param_2 = obj.get(key,timestamp);
  */
+
+/** ----------- TreeMap ---------- */
+//  class TimeMap {
+//     private Map<String, TreeMap<Integer, String>> m;
+
+//     public TimeMap() {
+//         m = new HashMap<>();    
+//     }
+    
+//     public void set(String key, String value, int timestamp) {
+//         m.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+//     }
+    
+//     public String get(String key, int timestamp) {
+//         if (!m.containsKey(key)) return "";
+//         TreeMap<Integer, String> timestamps = m.get(key);
+//         Map.Entry<Integer, String> entry = timestamps.floorEntry(timestamp);
+
+//         return entry == null ? "" : entry.getValue();
+//     }
+
+//     // public String get(String key, int timestamp) {
+//     //     return Optional.ofNullable(m.get(key))
+//     //                     .map(t -> t.floorEntry(timestamp))
+//     //                     .map(Map.Entry::getValue)
+//     //                     .orElse("");
+//     // }
+// }
