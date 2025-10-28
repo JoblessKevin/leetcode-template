203d5535527dbeb809ac2b82763bedd0ced19c7c
KevinChiu <kevinhyd108@gmail.com>
2025-10-28 12:54:34 +0800

    Add LinkedList

---
+++ src/main/java/problems/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,40 @@
+package problems.linkedlist;
+
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode newHead = head;
+        if (head.next != null) {
+            newHead = reverseList(head.next);
+            head.next.next = head;
+        }
+        head.next = null;
+
+        return newHead;
+    }
+
+    public ListNode reverseList_LinkedList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
+    }
+}
