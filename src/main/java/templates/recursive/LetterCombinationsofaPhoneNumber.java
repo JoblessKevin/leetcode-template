22934fdfe464246323a496df0758dae192dd8440
JoblessKevin <152775911+JoblessKevin@users.noreply.github.com>
2025-11-05 22:44:41 +0800

    Update LetterCombinationsofaPhoneNumber.java

--- src/main/java/templates/recursive/LetterCombinationsofaPhoneNumber.java
+++ src/main/java/templates/recursive/LetterCombinationsofaPhoneNumber.java
@@ -1,4 +1,4 @@
-package main.java.templates.recursive;
+package templates.recursive;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,13 +10,14 @@ class LetterCombinationsofaPhoneNumber {
         "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
     };
 
-    // public List<String> letterCombinations(String digits) {
-    //     List<String> result = new ArrayList<>();
-    //     if (digits == null || digits.isEmpty()) return result;
+    /** Recursive */
+     public List<String> letterCombinations(String digits) {
+         List<String> result = new ArrayList<>();
+         if (digits == null || digits.isEmpty()) return result;
 
-    //     backtrack(digits, 0, new StringBuilder(), result);
-    //     return result;
-    // }
+         backtrack(digits, 0, new StringBuilder(), result);
+         return result;
+     }
 
     private void backtrack(String digits, int idx, StringBuilder path, List<String> result) {
         if (idx == digits.length()) {
@@ -52,7 +53,7 @@ class LetterCombinationsofaPhoneNumber {
     }
 
     /** Improved Stream API */
-    public List<String> letterCombinations(String digits) {
+    public List<String> letterCombinations_streamApi_improved(String digits) {
         if (digits == null || digits.length() == 0) return Collections.emptyList();
 
         List<StringBuilder> result = new ArrayList<>();
