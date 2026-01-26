5f4d769c69f3f8c42460eb6cd71b77f850c0df48
KevinChiu <kevinhyd108@gmail.com>
2026-01-26 08:53:18 +0800

    Add Palindrome backtracking

---
+++ src/main/java/problems/backtracking/PalindromePartitioning.java
@@ -0,0 +1,35 @@
+package problems.backtracking;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), s, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, List<String> tempList, String s, int start) {
+        if (start == s.length()) {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s, start, i)) {
+                tempList.add(s.substring(start, i + 1));
+                backtrack(result, tempList, s, i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
