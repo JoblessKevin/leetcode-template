bf042cfb77903dcf9b87ed6cd2c39c9bb40f6d06
KevinChiu <kevinhyd108@gmail.com>
2026-03-09 09:49:35 +0800

    Add topological sort template

---
+++ src/main/java/templates/topologySort/TopologicalSortDFS.java
@@ -0,0 +1,45 @@
+package templates.topologySort;
+
+import java.util.*;
+
+public class TopologicalSortDFS {
+
+    public List<Integer> topologicalSort(int numTasks, List<List<Integer>> adj) {
+        int[] visited = new int[numTasks];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < numTasks; i++) {
+            if (visited[i] == 0) {
+                if (hasCycle(i, adj, visited, stack)) {
+                    return new ArrayList<>();
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        return result;
+    }
+
+    private boolean hasCycle(int curr, List<List<Integer>> adj, int[] visited,
+                                    Stack<Integer> stack) {
+        visited[curr] = 1;
+
+        for (int neighbor : adj.get(curr)) {
+            if (visited[neighbor] == 1) {
+                return true;
+            }
+            if (visited[neighbor] == 0) {
+                if (hasCycle(neighbor, adj, visited, stack)) {
+                    return true;
+                }
+            }
+        }
+
+        visited[curr] = 2;
+        stack.push(curr);
+        return false;
+    }
+}
---
+++ src/main/java/templates/topologySort/TopologicalSortKahn.java
@@ -0,0 +1,43 @@
+package templates.topologySort;
+
+import java.util.*;
+
+public class TopologicalSortKahn {
+
+    public List<Integer> topologicalSort(int numTasks, List<List<Integer>> adj) {
+        int[] inDegree = new int[numTasks];
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < numTasks; i++) {
+            for (int neighbor : adj.get(i)) {
+                inDegree[neighbor]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numTasks; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            result.add(curr);
+
+            for (int neighbor : adj.get(curr)) {
+                inDegree[neighbor]--;
+
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        if (result.size() != numTasks) {
+            return new ArrayList<>();
+        }
+
+        return result;
+    }
+}
